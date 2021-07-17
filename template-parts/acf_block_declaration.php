1c81321244b350835f904f6e7d22614d48a23769
NaNathan13 <natewilson135@gmail.com>
2021-07-17 00:23:35 -0400

    quick fix

--- template-parts/acf_block_declaration.php
+++ template-parts/acf_block_declaration.php
@@ -5,7 +5,7 @@ $pageContent = get_field('page_content');
 
 if (have_rows($pageContent)) :
     while (have_rows($pageContent) : the_row();
-        // Content
+
         if (get_row_layout() == 'content--image') :
 
             get_template_part('template-parts/components/blocks/content/content--image');
@@ -23,12 +23,12 @@ if (have_rows($pageContent)) :
             get_template_part('template-parts/components/blocks/content/content--wysiwyg');
 
         elseif (get_row_layout() == 'hero') :
+            get_template_part('template-parts/components/blocks/content/content--wysiwyg');
 
-        // Sections
         elseif (get_row_layout() == 'columns-content') :
 
-            get_template_part('template-parts/components/blocks/sections/columns-content');
-
+            get_template_part('template-parts/components/blocks/heros/hero');
+            
         endif;
     endwhile;
 endif;
