685807f6c737cb5ccb8126ac159e9817e933e055
NaNathan13 <natewilson135@gmail.com>
2021-07-27 22:52:50 -0400

    Bunch of fixes for about

--- about.php
+++ about.php
@@ -13,6 +13,8 @@ namespace WP_Rig\WP_Rig;
 
 get_header();
 get_template_part('template-parts/javascript/navigationJs');
+get_template_part('template-parts/javascript/expandableRowsShortcodeJs');
+
 
 $centerValue = get_field('site_name', 'option');
 
@@ -25,14 +27,14 @@ $centerValue = get_field('site_name', 'option');
         <!-- Content Left Drawing Right -->
         <section class="about_page_location_section">
             <div class="about_page_location_container">
-                <h2><?php echo get_field('about_page_-_location_headline') ?></h2>
+                <h2><?= get_field('about_page_-_location_headline') ?></h2>
                 <div class="about_page_location_wrapper">
                     <div class="about_page_location_content_wrapper">
-                        <?php echo get_field('about_page_-_location_content_wysiwyg') ?>
+                        <?= get_field('about_page_-_location_content_wysiwyg') ?>
                     </div>
 
                     <div class="about_page_location_iframe_wrapper">
-                        <img src="<?php echo get_field('about_page_center_drawn_image')['url'] ?>" alt="" class="about_page_center_image">
+                        <img src="<?= get_field('about_page_center_drawn_image')['url'] ?>" alt="" class="about_page_center_image">
                     </div>
                 </div>
             </div>
@@ -41,14 +43,14 @@ $centerValue = get_field('site_name', 'option');
         <!-- Treatment List -->
         <section class="about_page_treatment_list_section">
             <div class="about_page_treatment_list_container" <?= !empty(get_field('about_page_treatment_list_background_image')) ? 'style= url(' . get_field('about_page_treatment_list_background_image') .  ');' : '' ?>>
-                <h2><?php echo get_field('about_page_treatment_list_items_headline') ?></h2>
+                <h2><?= get_field('about_page_treatment_list_items_headline') ?></h2>
                 <ul>
                     <?php
                     if (have_rows('about_page_treatment_list_items')) :
                         while (have_rows('about_page_treatment_list_items')) : the_row();
                     ?>
                             <li>
-                                <img src="<?php echo get_template_directory_uri() . '/icons/checkmark_icon.png' ?>" alt="" loading="lazy"> <?php echo get_sub_field('text') ?>
+                                <img src="<?= get_template_directory_uri() . '/icons/checkmark_icon.png' ?>" alt="" loading="lazy"> <?= get_sub_field('text') ?>
                             </li>
                     <?php
                         endwhile;
@@ -59,57 +61,61 @@ $centerValue = get_field('site_name', 'option');
         </section>
 
 
+        <!-- Expandable Row and Map -->
+        <section class="about_page_expandable_row_section">
+            <div class="about_page_expandable_row_container">
+                <div class="about_page_expandable_row_wrapper">
+                    <?php
+                    if (have_rows('about_page_expandable_rows')) :
+                        while (have_rows('about_page_expandable_rows')) : the_row();
+                    ?>
+                            <div class="expandable_row_element">
+                                <div class="expandable_row_element_headline_img_wrapper">
+                                    <h5><?= get_sub_field('headline') ?></h5>
+                                    <img src="<?= get_field('plus_icon', 'option')['url'] ?>" alt="">
+                                </div>
 
-        <!-- Expandable Rows and Location iframe -->
-        <section class="location-and-accordion" style="<?= !empty($backgroundImage) ? "background-image:url({$backgroundImage}) !important;" : "" ?>">
-            <div class="row wrapper">
-                <div class="col-xs-12 col-md-5">
-                    <div class="center_location flex flex-column justify-center">
-                        <iframe src="<?= $iframe ?>" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                                <div class="expandable_row_content hide">
+                                    <p><?= get_sub_field('content') ?></p>
+                                    <a href=""></a>
+                                </div>
+                            </div>
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
+                </div>
+                <div class="about_page_expandable_row_location_container">
+                    <iframe src="<?= get_field('about_page_iframe') ?>" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
-                        <a href="<?= $link['url'] ?>" style="<?= !empty($addressBackgroundColor) ? "background-color:{$addressBackgroundColor} !important;" : "" ?>"><span>Address: </span><?= $address ?></a>
+                    <div class="about_page_location_address">
+                        <a href="<?= get_field('about_page_address_url')['url'] ?>" target="<?= get_field('about_page_address_url')['target'] ?>"><span>Address: </span><?= get_field('center_address', 'option') ?></a>
                     </div>
                 </div>
-                <div class="col-xs-12 col-md-7 flex flex-column justify-center">
-                    <?php foreach ($accordions as $accordion) : ?>
-                        <div class="accordion" style="<?= !empty($borderColor) ? "border-color:{$borderColor} !important;" : "" ?>">
-                            <div class="accordion_top">
-                                <h5 style="<?= !empty($borderColor) ? "color:{$borderColor} !important;" : "" ?>"><?= $accordion['headline'] ?></h5>
-                                <img class="accordion_icon" src="<?php echo get_field('plus_icon', 'option')['url'] ?>" loading="lazy" alt="Plus icon">
-                            </div>
-
-                            <div class="accordion_bottom flex flex-column">
-                                <p><?= $accordion['content'] ?></p>
-
-                                <?php if ($accordion['add_cta']) : ?>
-                                    <?php if ($accordion['cta_type'] == "link") : ?>
-                                        <a class="button" href="<?= $accordion['link']['url'] ?>" target="<?= $accordion['link']['target'] ?>"><?= $accordion['link']['title'] ?></a>
-                                    <?php else : ?>
-                                        <a class="invocaNumber button" href="tel:949-276-2886">949-276-2886</a>
-                                <?php endif;
-                                endif; ?>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            </div>
+            </div>`
         </section>
 
         <!-- Treatment Step Carousel -->
-        <?php echo do_shortcode('[treatment_step_carousel]') ?>
+        <?= do_shortcode('[treatment_step_carousel]') ?>
 
+
+        <?php
+
+        $about_testimonials = get_field('about_page_testimonials');
+
+        ?>
         <!-- Accreditation and Testimonial -->
-        <section class="about_page_testimonial_section">
+        <section class="about_page_testimonial_section accreditation_and_testimonial--testimonial">
             <div class="about_page_badge_container">
                 <h2>Our Accreditation</h2>
                 <div class="center_page_badges_wrapper">
-                    <?php echo get_field('legitscript_html', 'option') ?>
+                    <?= get_field('legitscript_html', 'option') ?>
 
                     <?php
                     if (have_rows('footer_badges', 'option')) :
                         while (have_rows('footer_badges', 'option')) : the_row();
                     ?>
-                            <img loading="lazy" src="<?php echo get_sub_field('badge', 'option')['url']; ?>" alt="">
+                            <img loading="lazy" src="<?= get_sub_field('badge', 'option')['url']; ?>" alt="">
                     <?php
                         endwhile;
                     endif;
@@ -117,12 +123,24 @@ $centerValue = get_field('site_name', 'option');
                 </div>
             </div>
             <div class="about_page_testimonial_container">
-                <p><?php echo get_field('about_page_testimonial') ?></p>
-                <p><?php echo get_field('about_page_testimonial_name') ?></p>
-                <img src="<?php echo get_template_directory_uri() . '/icons/five_stars.png' ?>" alt="" loading="lazy">
+                <?php foreach ($about_testimonials as $testimonial) : ?>
+                    <div class="carousel-slide">
+                        <?= $testimonial['testimonial']; ?>
+                    </div>
+                <?php endforeach; ?>
+
+                <img src="<?= get_template_directory_uri() . '/icons/five_stars.png' ?>" alt="" loading="lazy">
+                <?php if (count($about_testimonials) > 1) : ?>
+                    <div style="text-align:center">
+                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                        <?php foreach ($testimonials as $testimonial) : ?>
+                            <span class="dot" onclick="currentSlide(<?= $testimonialSlideNumber++ ?>)"></span>
+                        <?php endforeach; ?>
+                        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                    </div>
+                <?php endif; ?>
             </div>
         </section>
-
     </div>
 </main> <?php
         get_footer();
--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+var slideIndex = 1;
+showSlides (slideIndex);
+
+function plusSlides (n) {
+  console.log ('plus clicked');
+
+  showSlides ((slideIndex += n));
+}
+
+function currentSlide (n) {
+  showSlides ((slideIndex = n));
+}
+
+function showSlides (n) {
+  let i;
+  let slides = document.getElementsByClassName ('carousel-slide');
+  let dots = document.getElementsByClassName ('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace (' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
 // function validateFooterForm() {
 //   let e = document.querySelector(".footer_form_fn").value,
 //     t = document.querySelector(".footer_form_ln").value,
@@ -417,76 +450,3 @@ jQuery(document).ready(function (e) {
 jQuery(document).ready(function ($) {
     // Image CTA Headline Sub Headline Two Buttons
 });
-
-jQuery (document).ready (function ($) {
-  $ ('.accordion').click (function (event) {
-    event.stopImmediatePropagation ();
-    $ (this).find ('.accordion_icon').toggleClass ('dropdown_active');
-    $ (this).find ('.accordion_bottom').toggleClass ('show');
-  });
-});
-
-var slideIndex = 1;
-showSlides (slideIndex);
-
-function plusSlides (n) {
-  console.log ('plus clicked');
-
-  showSlides ((slideIndex += n));
-}
-
-function currentSlide (n) {
-  showSlides ((slideIndex = n));
-}
-
-function showSlides (n) {
-  let i;
-  let slides = document.getElementsByClassName ('carousel-slide');
-  let dots = document.getElementsByClassName ('dot');
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace (' active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
-
-var slideIndex = 1;
-showSlides (slideIndex);
-
-function plusSlides (n) {  
-  showSlides ((slideIndex += n));
-}
-
-function currentSlide (n) {
-  showSlides ((slideIndex = n));
-}
-
-function showSlides (n) {
-  let i;
-  let slides = document.getElementsByClassName ('carousel-slide');
-  let dots = document.getElementsByClassName ('dot');
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace (' active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
-
