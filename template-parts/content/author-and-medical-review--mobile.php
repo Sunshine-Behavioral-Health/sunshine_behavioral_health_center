256426518cdff10910ff7fbfc3a204c0c6498362
NaNathan13 <natewilson135@gmail.com>
2021-08-04 23:09:27 -0400

    author and medical revew - css

--- template-parts/content/author-and-medical-review--mobile.php
+++ template-parts/content/author-and-medical-review--mobile.php
@@ -12,16 +12,16 @@ $medicalReviewerMetaData = get_user_meta($medicalReviewUser['ID']);
 
 ?>
 <?php if (get_field('editor_user') || get_field('medical_review_user')) : ?>
-    <div class="author-and-medical-review author-and-medical-review--mobile">
+    <div class="author-and-medical-review author-and-medical-review--mobile flex flex-column align-center">
         <?php if (get_field("editor_user")) : ?>
             <div class="author_container flex m-b-15">
                 <div class="author_content flex flex-column">
                     <div class="flex">
-                        <p class="last-edited">Last Edited:</p>
+                        <p class="last-edited m-r-15">Last Edited:</p>
                         <p class="date"><?= get_field('last_edited_date'); ?></p>
                     </div>
                     <div class="flex">
-                        <p class="title">Author:</p>
+                        <p class="title m-r-15">Author:</p>
                         <a class="name" href="/contributors#<?= $AuthorMetaData['first_name'][0] . '_' . $AuthorMetaData['last_name'][0] ?>"><?= $authorUser['display_name'] ?></a>
                     </div>
                 </div>
