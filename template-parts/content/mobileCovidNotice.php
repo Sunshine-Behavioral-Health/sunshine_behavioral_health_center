f00aa7ca825d0c6477fa877fd3ded785668f5263
NaNathan13 <natewilson135@gmail.com>
2021-08-04 23:35:56 -0400

    DEV-153 Reduced media query in hero.scss

--- template-parts/content/mobileCovidNotice.php
+++ template-parts/content/mobileCovidNotice.php
@@ -1,4 +1,4 @@
-<section class="mobile_covid_notice_section hide_on_desktop">
+<section class="mobile_covid_notice_section hide_on_desktop <?php !empty(get_field('editor_user') || get_field('medical_review_user')) ? "m-b-15" : ""; ?>">
     <div class="mobile_covid_notice_container">
         <a href="/our-response-to-the-corona-virus-health-concern/">Our Response to COVID-19</a>
     </div>
